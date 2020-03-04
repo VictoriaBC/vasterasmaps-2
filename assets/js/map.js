c0bec19a79280d337ccf6a5982027c1325e66935
VictoriaBC <40825908+VictoriaBC@users.noreply.github.com>
2020-03-04 17:20:17 -0400

    Add files via upload

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,103 +1,44 @@
-var map
-var markers = []
-// start out with filter features set to false, so no filtering happens by default
-var filters = {restaurant:false, hotel:false, attraction:false}
+/* Google Map */
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15, 
+        center: {
+            lat: 59.6119248,
+            lng: 16.5447214
+        }
+    });
+/* Map Markers and information */
+/* Map Markers and information string 1 */
+            var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Djäkneberget park</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Djäkneberget</b> is a park area directly west of the inner city of Västerås.'+ 
+            'The area is about 500 meters long and 250 meters wide (about twelve hectares).'+ 
+            'The park is located on a hilltop with views of the Cathedral, '+
+            'Stadshuset, Skrapan and part of Lake Mälaren. Adjacent to the park is Djäknebergsskolan, '+
+            'a public school that calls for 1894 and which today houses Västerås Folk High School '+
+            'and Västerås Art School.</p>'+
+            '<p>Attribution: Djäkneberget, (translated to english) <a href="https://sv.wikipedia.org/wiki/Dj%C3%A4kneberget">'+
+            'https://sv.wikipedia.org/wiki/Dj%C3%A4kneberget</a> '+
+            '</div>'+
+            '</div>';
 
-$(function () {
-    $('input[name=filter]').change(function (e) {
-     map_filter(this.id);
-      filter_markers()
-  });
-
-
-})
-
-var get_set_options = function() {
-  ret_array = []
-  for (option in filters) {
-    if (filters[option]) {
-      ret_array.push(option)
-    }
-  }
-  return ret_array;
-}
-
-var filter_markers = function() {  
-  set_filters = get_set_options()
-  
-  // for each marker, check to see if all required options are set
-  for (i = 0; i < markers.length; i++) {
-    marker = markers[i];
-
-    // start the filter check assuming the marker will be displayed
-    // if any of the required features are missing, set 'keep' to false
-    // to discard this marker
-    keep=true
-    for (opt=0; opt<set_filters.length; opt++) {
-      if (!marker.properties[set_filters[opt]]) {
-        keep = false;
-      }
-    }
-    marker.setVisible(keep)
-  }
-}
-
-var map_filter = function(id_val) {
-   if (filters[id_val]) 
-      filters[id_val] = true
-   else
-      filters[id_val] = false
-}
-
-
-// after the geojson is loaded, iterate through the map data to create markers
-// and add the pop up (info) windows
-function loadMarkers() {
-  console.log('creating markers')
-  var infoWindow = new google.maps.InfoWindow()
-  geojson_url = 'https://raw.githubusercontent.com/gizm00/blog_code/master/appendto/python_maps_2/collection.geojson'
-  $.getJSON(geojson_url, function(result) {
-      // Post select to url.
-      data = result['features']
-      $.each(data, function(key, val) {
-        var point = new google.maps.LatLng(
-                parseFloat(val['geometry']['coordinates'][1]),
-                parseFloat(val['geometry']['coordinates'][0]));
-        var titleText = val['properties']['title']
-        var descriptionText = val['properties']['description']
-        var marker = new google.maps.Marker({
-          position: point,
-          title: titleText,
-          map: map,
-          properties: val['properties']
-         });
-
-        var markerInfo = "<div><h3>" + titleText + "</h3>Amenities: " + descriptionText + "</div>"
-
-
-        marker.addListener('click', function() {
-           $('#campground_info').html(markerInfo)
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
         });
-        markers.push(marker)
-        
-      });
-  });
-}
+        var image = 'http://victoriapben.com/wp-content/uploads/2020/03/nature.jpg';
+        var marker = new google.maps.Marker({
+          position: { lat: 59.61153, lng: 16.530734 },
+          map: map,
+          icon: image,
+          title: 'Djäkneberget'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
 
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: 59.6119248,
-                    lng: 16.5447214
-                }
-            });
 
-    
-    map_document = document.getElementById('map')
-    map = new google.maps.Map(map_document,map_options);
-    loadMarkers()
- 
-}
 
-google.maps.event.addDomListener(window, 'load', initMap);
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });}
